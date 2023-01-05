7401a7a5f152a91190e9197b925f3a3e9efa5265
Felix <stenbackfelix@gmail.com>
2023-01-05 15:43:32 +0100

    prints current guitar string, sort of

--- tune.py
+++ tune.py
@@ -1,12 +1,44 @@
-import wave
-import time
-
-import pyaudio
 import audioop
+import numpy as np
+import pyaudio
+import time
+import wave
+
+RATE = 44100
+CHANNEL = 2
+CHUNK = 2048
+GUITAR_STRING_FREQS = {
+    "E_1": 82,
+    "B_2": 110,
+    "G_3": 146,
+    "D_4": 196,
+    "A_5": 247,
+    "E_6": 330,
+}
 
 def callback(in_data, frame_count, time_info, status):
     """ callback for playback data """
-    print(audioop.rms(in_data, 2))
+    np_data = np.fromstring(in_data, dtype=np.int16)
+    p = 20*np.log10(np.abs(np.fft.rfft(np_data)))
+    f = np.linspace(0, RATE/2.0, len(p))
+    pair_pf = zip(p, f)
+
+    amp = -1
+    fq = 0
+    for ppf in pair_pf:
+        if ppf[0] > amp:
+            amp = ppf[0]
+            fq = ppf[1]
+
+    string = ""
+    dst = len(p)
+    for gs, gfq in GUITAR_STRING_FREQS.items():
+        dfq = abs(gfq - fq)
+        if abs(gfq - fq) < dst:
+            dst = dfq
+            string = gs
+    print("String: ", string)
+
     # If len(data) is less than requested frame_count, PyAudio automatically
     # assumes the stream is finished, and the stream stops.
     return (in_data, pyaudio.paContinue)
@@ -17,10 +49,10 @@ def main():
 
     # Open stream using callback (3)
     stream = p.open(format=pyaudio.paInt16,
-                    channels=2,
-                    rate=44100,
+                    channels=CHANNEL,
+                    rate=RATE,
                     input=True,
-                    frames_per_buffer=1024,
+                    frames_per_buffer=CHUNK,
                     stream_callback=callback)
 
     # Wait for stream to finish (4)
@@ -33,5 +65,5 @@ def main():
     # Release PortAudio system resources (6)
     p.terminate()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
